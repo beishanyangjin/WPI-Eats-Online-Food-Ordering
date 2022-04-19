4b3a7f57bdba41e3fd08a4a0763bb241c689b142
csmbrad <58525771+csmbrad@users.noreply.github.com>
2022-04-19 02:29:36 -0400

    example of how to get data from the db, start API with search ability

--- server.js
+++ server.js
@@ -34,7 +34,6 @@ function testMySQL2() {
 
     connection.end()
 }
-
 testMySQL2()
 
 // enable strict routing and use slash:
@@ -45,6 +44,9 @@ app.use(slash());
 app.use(express.static("Public"));
 app.use(express.static("Views"));
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Routes
+
 // just send the basic index html. this is used because both / and /index can go to index.
 function getIndex(req, res) {
     res.sendFile(__dirname + "index.html");
@@ -53,6 +55,48 @@ function getIndex(req, res) {
 app.get("/", (req, res)      => {getIndex(req, res); });
 app.get("/index", (req, res) => {getIndex(req, res); });
 
+app.get("/query", (req, res) => {
+    const query = mysql.escape(req.body.query);
+    let result = {'restaurants': [], 'foods': []};
+
+    connection.connect();
+    // find restaurants with names like the query text
+    connection.query(
+        'SELECT R.R_name as rname, RT.type_Name as rtype, R.R_Image_Reference as img FROM restaurant R NATURAL JOIN restaurant_type RT WHERE R_name LIKE ?',
+        [`%${query}%`],
+        (err, rows, fields) => {
+            // add each restaurant to the result array
+            if (err) throw err;
+            rows.forEach(element => {
+                result.restaurants.push({
+                    'R_name': element.rname,
+                    'Category': element.rtype,
+                    'R_Image_Reference': element.img
+                    })
+            });
+    })
+    // find foods the same way
+    connection.query(
+        'SELECT F.F_name as fname, FT.type_Name as ftype, F.F_description as fdescription, F.F_Image_Reference as img FROM food F NATURAL JOIN food_type FT WHERE F_name LIKE ?',
+        [`%${query}%`],
+        (err, rows, fields) => {
+            // add each restaurant to the result array
+            if (err) throw err;
+            rows.forEach(element => {
+                result.foods.push({
+                    'F_name': element.fname,
+                    'F_type': element.ftype,
+                    'F_description': element.fdescription,
+                    'F_Image_Reference': element.img
+                })
+            });
+        })
+    connection.end();
+    res.json(result);
+})
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Listener
 
 const listener = app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
