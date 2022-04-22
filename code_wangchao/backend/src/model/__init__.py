d9ce39e56c71da67ebabf22174b3f932b96f9a1c
csmbrad <58525771+csmbrad@users.noreply.github.com>
2022-04-22 17:07:42 -0400

    Delete code_wangchao/backend/src directory

--- code_wangchao/backend/src/model/__init__.py
+++
@@ -1,5 +0,0 @@
-import os
-import sys
-
-dname = os.path.dirname(__file__)
-sys.path.append(os.path.dirname(dname))
--- code_wangchao/backend/src/routers/user.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-from controller import user_controller 
-
-router_user = Blueprint('user', __name__)
-
-router_user.route('/register', methods=['POST'])(user_controller.register)
